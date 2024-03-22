9d13bb03bfa98d62770c926eaaccbd0a6b2f6072
dantt3x <josephhayman1213@gmail.com>
2024-03-21 23:18:53 -0400

    camera smooth

--- src/client/Camera.lua
+++ src/client/Camera.lua
@@ -4,28 +4,37 @@ local Camera = {}
 Camera.__index = Camera
 
 local RunService = game:GetService("RunService")
---local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
+local SpringModule = require(ReplicatedStorage.Shared.Spring)
 local localPlayer = Players.LocalPlayer
-local DefaultOffset = Vector3.new(0, 3, 18)
+local DefaultOffset = Vector3.new(0, 3, 25)
 
 function Camera.Init()
     local newCamera = {}
     newCamera.enabled = true
     newCamera.currentCamera = game.Workspace.CurrentCamera
     newCamera.playerCharacter = localPlayer.Character
+
+    newCamera.xSpring = SpringModule.fromFrequency(10, 1, 5, 0, 0, 0)
+    newCamera.xSpring:SnapToCriticalDamping()
+
+    newCamera.ySpring = SpringModule.fromFrequency(10, 1, 5, 0, 0, 0)
+    newCamera.ySpring:SnapToCriticalDamping()
+
     return setmetatable(newCamera, Camera)
 end
 
 function Camera:Start()
-    self.Camera = RunService:BindToRenderStep("Camera", Enum.RenderPriority.Camera.Value + 1, function (deltaTime)
+    self.Camera = RunService.Stepped:Connect(function(time, deltaTime) 
         if self.enabled == false then 
             return
         end
 
         if not self.currentCamera then
             self.currentCamera = game.Workspace.CurrentCamera
+            self.currentCamera.CameraType = Enum.CameraType.Scriptable
         end
 
         if not self.playerCharacter then
@@ -34,19 +43,55 @@ function Camera:Start()
             end
         end
 
-        local primaryPart = self.playerCharacter.PrimaryPart
         self.currentCamera.CameraType = Enum.CameraType.Scriptable
-        self.currentCamera.CFrame = CFrame.lookAt(primaryPart.Position + DefaultOffset, primaryPart.Position)
+
+        local primaryPart = self.playerCharacter.PrimaryPart
+        --print(self.xSpring.Offset, self.xSpring.Goal)
+        --print(self.ySpring.Offset, self.ySpring.Goal)
+
+        local finalPosition = Vector3.new(self.xSpring.Offset, self.ySpring.Offset, primaryPart.Position.Z + DefaultOffset.Z)
+        self.currentCamera.CFrame = CFrame.new(finalPosition)
+
+        self.xSpring:SetOffset(self.currentCamera.CFrame.Position.X)
+        self.xSpring:SetGoal(primaryPart.Position.X + (self.playerCharacter.Humanoid.MoveDirection.X * 2.5))
+        self.ySpring:SetOffset(self.currentCamera.CFrame.Position.Y)
+        self.ySpring:SetGoal(primaryPart.Position.Y + DefaultOffset.Y)
+
+        --print(self.xSpring:SetOffset(1 * deltaTime))
+        
+        
+
+        --local springOffset = Vector3.new(-self.currentSpring.Offset,0,0)
+        --print(self.currentSpring.Offset, self.currentSpring.Goal)
+        --self.currentSpring.Goal = 200
+
+        --[[
+        self.currentCamera.CFrame = CFrame.lookAt(
+            (Vector3.new(self.currentSpring.Offset, primaryPart.Position.Y, primaryPart.Position.Z) + DefaultOffset), 
+            Vector3.new(self.currentSpring.Offset, primaryPart.Position.Y, primaryPart.Position.Z)
+        )]]
+
+
+
+        --self.currentSpring:SetOffset(self.lastPosition or 0)
+        --self.currentSpring:SetGoal( primaryPart.Position.X)
+
+        --self.lastPosition = primaryPart.Position.X
+        --self.currentSpring:Reset()
     end)
+
+    self.Spring = RunService.Stepped:Connect(function(time, deltaTime)
+                
+    end)
+end
+
+function Camera:SetEnabled(isEnabled: boolean)
+    self.enabled = isEnabled
 end
 
 function Camera:Clean()
     self.Camera:Disconnect()
     self = nil
-end
-
-function Camera:SetEnabled(isEnabled: boolean)
-    self.enabled = isEnabled
 end
 
 return Camera
